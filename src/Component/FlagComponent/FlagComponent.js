c11985acf6798a0be30ce2f1ff89ee6ee234db2d
Parvez <parvez@studiometrodesk.com>
2023-03-20 12:52:13 +0600

    Flags Js Data Added

---
+++ src/Component/FlagComponent/FlagComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlagComponent = () => {
+    return (
+        <div className='container mx-auto'>
+            <h2 className='my-24 text-3xl font-semibold text-gray-700 text-start px-56'>Serving Globally</h2>
+        </div>
+    );
+};
+
+export default FlagComponent;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FlagComponent from '../../Component/FlagComponent/FlagComponent';
 import HomeComponent from '../../Component/HomeComponent/HomeComponent';
 
 const HomePage = () => {
     return (
         <div>
             <HomeComponent/>
+            <FlagComponent/>
         </div>
     );
 };
