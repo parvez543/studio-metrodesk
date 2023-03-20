63ed4119af2ccf52a0bb4543782f47e58dee68f8
Parvez <parvez@studiometrodesk.com>
2023-03-20 17:05:21 +0600

    Service Card Added

--- src/Component/ServiceComponent/ServiceComponent.js
+++ src/Component/ServiceComponent/ServiceComponent.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./ServiceComponent.css"
+import serviceData from "../../data/serviceData"
 
 const ServiceComponent = () => {
+    
     return (
         <div className='container mx-auto py-20 lg:px-56 '>
             <h2 className='text-3xl md:text-4xl lg:text-5xl text-left serviceText font-semibold mb-3'>Photo Editing Services </h2>
             <p className='text-left md:text-lg text-gray-700'>Let us look after the edits, so you can get back to the work that needs you. You can always <span className='markText'>get up to 20% discount</span> during order placement when selecting a longer turnaround time.</p>
-
+            <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-4 lg:gap-8 mt-12'>
+                {
+                    serviceData?.map((sData, id) => <>
+                        <div className='serviceCard'>
+                            <h2 className='text-2xl font-semibold text-gray-800'>{sData.title}</h2>
+                            <p className='text-gray-500 text-xl mt-2 mb-5 '>{sData.description}</p>
+                            <p className='text-blue-500 flex text-lg justify-center hover:text-blue-800 mb-6'>{sData.CTA}</p>
+                            <div className='flex justify-end'>
+                                <img className='serviceImg' src={sData.img} alt="Service Images"/>
+                            </div>
+                        </div>
+                    </>)
+                }
+            </div>
         </div>
     );
 };
--- src/data/serviceData.js
+++ src/data/serviceData.js
@@ -44,3 +44,5 @@ const services = [
     
     
 ]
+
+export default services;
