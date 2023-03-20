c52181fdb68f47169365374b427442a253a89f1d
Parvez <parvez@studiometrodesk.com>
2023-03-20 16:11:27 +0600

    Flag Section Rearranged

--- src/Component/FlagComponent/FlagComponent.js
+++ src/Component/FlagComponent/FlagComponent.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import UKFlag from "../../assets/Flages/united-kingdom.png"
-import USAFlag from "../../assets/Flages/USA.png"
-import CanadaFlag from "../../assets/Flages/canada.png"
-import GermanFlag from "../../assets/Flages/germany.png"
-import AustraliaFlag from "../../assets/Flages/australia.png"
-import SwedenFlag from "../../assets/Flages/sweden.png"
+import flags from "../../assets/images/clients-flag.png"
 import "./FlagComponent.css"
 
 
@@ -15,19 +10,16 @@ const FlagComponent = () => {
 
     return (
         <div className='heroSection'>
-            <div className='container mx-auto lg:py-14'>
-                <h2 className='text-3xl font-semibold text-gray-700 text-start px-10 lg:px-56'>Serving Globally</h2>
-                <div className='grid grid-cols-7 gap-4 lg:px-56 justify-center justify-items-center px-10 items-center mt-6'>
-                    <img src={UKFlag} alt="Flag of United Kingdom" />
-                    <img src={USAFlag} alt="Flag of USA" />
-                    <img src={CanadaFlag} alt="Flag of Canada" />
-                    <img src={GermanFlag} alt="Flag of Gremany" />
-                    <img src={AustraliaFlag} alt="Flag of Australia" />
-                    <img src={SwedenFlag} alt="Flag of Sweden" />
-                    <div className=''>
-                        <p className='text-xs lg:text-xl lg:-ml-11'>And More</p>
-                    </div>
-                </div>
+            <div className='container px-10 lg:px-0 mx-auto lg:py-14'>
+                <h2 className='text-3xl my-4 font-semibold text-gray-700 text-start px-10 lg:px-56'>Serving Globally</h2>
+               <div className='flex items-center gap-4 justify-center'>
+                   <div className=''>
+                        <img src={flags} alt="Clients Flag"/>
+                   </div>
+                   <div>
+                        <p className='text-xs lg:text-lg'>And More</p>
+                   </div>
+               </div>
             </div>
         </div>
     );
---
+++ src/data/serviceData.js
@@ -0,0 +1,27 @@
+const services = [
+    {
+        "id" : 1,
+        "title": "Clipping Path",
+        "description": " From $1/Per Image",
+        "CTA" : "Learn More >"
+    },
+    {
+        "id" : 2,
+        "title": "Multiple Clipping",
+        "description": " From $2/Per Image",
+        "CTA" : "Learn More >"
+    },
+    {
+        "id" : 2,
+        "title": "Ghost Mannequin",
+        "description": " From $2/Per Image",
+        "CTA" : "Learn More >"
+    },
+    {
+        "id" : 4,
+        "title": "Retouching",
+        "description": " From $5/Per Image",
+        "CTA" : "Learn More >"
+    },
+
+]
