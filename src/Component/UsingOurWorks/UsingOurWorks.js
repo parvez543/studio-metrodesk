9afbd2c525c323deb45c88551c3c6f6375b390d2
Parvez <parvez@studiometrodesk.com>
2023-03-21 17:08:26 +0600

    Global Brands Section Added

--- src/Component/UsingOurWorks/UsingOurWorks.js
+++ src/Component/UsingOurWorks/UsingOurWorks.js
@@ -13,6 +13,19 @@ const UsingOurWorks = () => {
                         </>)
                     }
                 </div>
+                <div className='flex flex-col lg:flex-row justify-around items-start mt-8'>
+                    <div className='mt-6'>
+                        <h2 className='ourWork mb-6 text-4xl lg:text-5xl text-left font-medium'>Global Brands That <br /> Use Our Work </h2>
+                    </div>
+                    <div className='lg:w-[550px] text-xl'>
+                        <div className='text-left'>
+                            <p className='mb-3 text-gray-700'>We are more than a photo editing company for many global brands, and they consider Studio Metrodesk as their post-production partner for success. Our comprehensive image editing services cover all aspects of professional product photos so that your product is the star of the show.</p>
+                        </div>
+                        <div className='text-left'>
+                            <p className='mb-3 text-gray-700'>We take our photo retouching job seriously, so no spec of dust shall be left behind! Let us show you what we can do with hand-drawn retouching from hair to watch faces.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
