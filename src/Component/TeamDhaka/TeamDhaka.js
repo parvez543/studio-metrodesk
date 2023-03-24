29fbc6504286d76e4f14626c8305f6923dcf340c
Parvez <parvez@studiometrodesk.com>
2023-03-24 16:15:15 +0600

    Dhaka Added

---
+++ src/Component/TeamDhaka/TeamDhaka.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./TeamDhaka.css"
+import dhaka from "../../data/teamDhaka"
+
+const TeamDhaka = () => {
+    return (
+        <div className='bg-[#F5F6FF]'>
+            <div className='container mx-auto px-10 lg:px-56 py-20'>
+                <h2 className='font-semibold text-left text-3xl md:text-5xl text-[#7A80BD]'>Team Dhaka</h2>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:px-56 lg:px-[355px] justify-items-center'>
+                {
+                    dhaka?.map(d => <>
+                        <div className=''>
+                        <img src={d.img} alt="Studio MetroDesk Team Dhaka Picture"/>
+                        <h2 className='text-left mt-2 font-semibold text-xl'>{d.name}</h2>
+                        <p className='text-left'>{d.designation}</p>
+
+                        </div>
+                    </>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default TeamDhaka;
--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LetsTalk from '../../Component/LetsTalk/LetsTalk';
 import MeetTheTeam from '../../Component/MeetTheTeam/MeetTheTeam';
+import TeamDhaka from '../../Component/TeamDhaka/TeamDhaka';
 
 const Team = () => {
     return (
         <div>
             <MeetTheTeam/>
+            <TeamDhaka/>
             <LetsTalk/>
         </div>
     );
