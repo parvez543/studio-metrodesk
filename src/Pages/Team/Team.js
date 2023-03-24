d4c7993d785b10a86ebc9d9e96d160b380bbc979
Parvez <parvez@studiometrodesk.com>
2023-03-24 16:04:33 +0600

    Let's Talk Added

--- src/Pages/Team/Team.js
+++ src/Pages/Team/Team.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LetsTalk from '../../Component/LetsTalk/LetsTalk';
 import MeetTheTeam from '../../Component/MeetTheTeam/MeetTheTeam';
 
 const Team = () => {
     return (
         <div>
             <MeetTheTeam/>
+            <LetsTalk/>
         </div>
     );
 };
