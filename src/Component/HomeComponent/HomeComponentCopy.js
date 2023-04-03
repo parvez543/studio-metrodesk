38e6934e2c587f87a84756113ed2f1a8021a57b4
Parvez <parvez@studiometrodesk.com>
2023-04-03 09:43:39 +0600

    Button Issue Fixed

--- src/Component/HomeComponent/HomeComponentCopy.js
+++ src/Component/HomeComponent/HomeComponentCopy.js
@@ -8,8 +8,8 @@ const HomeComponentCopy = () => {
                 <h2 className='text-white text-3xl lg:text-6xl font-bold '>Professional Photo Retouching <br/> Perfect for Photographers & Business</h2>
                 <p className='text-white text-sm lg:text-xl mt-10'>Twenty years of high-end retouching knowledgeable team, ready to work. <br/> Every industry has there own guidelines for product retouching, <br/> and our team works with almost all sectors that need complex product retouching.</p>
                 <div className='flex gap-8 justify-center items-center mt-10'>
-                    <button className='headerBtn lg:py-3 lg:px-20'>Free Trial</button>
-                    <button className='getAQuote lg:py-[13px] lg:px-20'>Get A Quote</button>
+                    <button className='headerBtn  px-10 py-3 lg:px-20'>Free Trial</button>
+                    <button className='getAQuote px-10 py-[13px] lg:px-20'>Get A Quote</button>
                 </div>
             </div>
         </div>
