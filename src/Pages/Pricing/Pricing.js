49869fc97409727598fe748d9f821e3afa6a16a1
Parvez <parvez@studiometrodesk.com>
2023-04-04 17:22:41 +0600

    Pricing Added

--- src/Pages/Pricing/Pricing.js
+++ src/Pages/Pricing/Pricing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Pricing.css"
 import PricingBanner from '../../Component/PricingBanner/PricingBanner';
+import PriceCalculator from '../../Component/PriceCalculator/PriceCalculator';
 
 const Pricing = () => {
     return (
         <div>
             <PricingBanner/>
+            <PriceCalculator/>
         </div>
     );
 };
