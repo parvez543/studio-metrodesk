6461f05a45920ef9ea64bb51203e8ea87cf003e2
Parvez <parvez@studiometrodesk.com>
2023-04-14 14:49:26 +0600

    Section Fixed

--- src/Component/ECommerceServiceInclude/ECommerceServiceInclude.js
+++ src/Component/ECommerceServiceInclude/ECommerceServiceInclude.js
@@ -4,45 +4,45 @@ import photoEditing from "../../assets/images/ClothPhotoEditing.jpg"
 
 const ECommerceServiceInclude = () => {
     return (
-        <div className='container mx-auto px-10 lg:px-56 py-20'>
+        <div className='container mx-auto px-10 lg:px-20 2xl:px-56 py-20'>
         <h2 className='mb-3 text-3xl sm:text-4xl md:text-5xl font-semibold text-[#7A80BD] text-left'>What Does Ecommerce Photo Editing Include?</h2>   
         <p className='text-lg text-gray-700 mb-3 text-left'>The main things in ecommerce image editing are color correction and background removal. To achieve desired results, you also need proper image cropping, brightness and contrast adjustment, tags removal, noise reduction, masking, clipping path, margin setting, watermarking, and more.</p> 
-    <div className='flex flex-col-reverse lg:flex-row gap-10 items-center'>
+    <div className='grid lg:grid gap-10 items-center'>
         <div>
             <h2 className='text-xl text-gray-900 my-6 text-left'>Ecommerce product photo editing services include:</h2>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Ecommerce website optimized editing</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Product photo retouching</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Clipping path and multi-clipping path</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Background color change / replacement</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Background color change / replacement</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Color adjustment and correction</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Remove unwanted shadows and add natural shadows</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>Reflection and glare elimination</p>
             </div>
-            <div className='flex gap-3 items-start justify-start lg:w-[650px]'>
+            <div className='flex gap-3 items-start justify-start'>
                 <FaCircle className='mt-[5px] w-[8px]' />
                 <p className='text-left text-xl'>White balance fixing, cropping and resizing</p>
             </div>
