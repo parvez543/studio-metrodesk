ae680b2e2654393609c3f555bff72d5a7617b6f3
Parvez <parvez@studiometrodesk.com>
2023-04-14 14:47:21 +0600

    Section Fixed

--- src/Component/ECommerceBusinessComponent/ECommerceBusinessComponent.js
+++ src/Component/ECommerceBusinessComponent/ECommerceBusinessComponent.js
@@ -5,8 +5,8 @@ import poster from "../../assets/images/parfect-for-Ecommerce-business.jpg"
 
 const ECommerceBusinessComponent = () => {
     return (
-        <div className='container mx-auto px-10 lg:px-56 py-20'>
-        <div className='flex flex-col lg:flex-row items-center justify-around gap-12'>
+        <div className='container mx-auto px-10 lg:px-20 2xl:px-56 py-20'>
+        <div className='grid lg:grid-cols-2 items-center justify-around gap-12'>
             <div>
                 <h2 className='page-title text-4xl md:text-9xl font-bold color-text text-left'>Ecommerce Image Editing Service</h2>
                 <p className='font-semibold text-xl md:text-2xl text-gray-900 mt-6 mb-1 text-left'>We strictly follow your brand guidelines and style — no AI or automation. Our team manually does every edit.</p>
