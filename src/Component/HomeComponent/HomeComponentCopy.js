f2a3a9e61af4cfb97cffacfe6e6275bed1186083
Parvez <parvez@studiometrodesk.com>
2023-04-17 16:46:14 +0600

    Home Banner Changed

--- src/Component/HomeComponent/HomeComponentCopy.js
+++ src/Component/HomeComponent/HomeComponentCopy.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import "./HomeComponent.css"
+import watch from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 1.png"
+import bag from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 2.png"
+import chair from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 1-1.png"
+import shoes from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 2-1.png"
+import tshirt from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 1-2.png"
+import newWatch from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 2-2.png"
+import children from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 1-3.png"
+import women from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 2-3.png"
+import sunglass from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 1-4.png"
+import jewellery from "../../assets/newImages/Homepage_Vol_1 (1)/Hero Section/Rectangle 2-4.png"
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
-const HomeComponentCopy = () => {
-    return (
-        <div className='homeContainer'>
-            <div className='px-10 lg:px-0 heading'>
-                <h2 className='text-white text-3xl lg:text-6xl font-bold '>Professional Photo Retouching <br/> Perfect for Photographers & Business</h2>
-                <p className='text-white text-sm lg:text-xl mt-10'>Twenty years of high-end retouching knowledgeable team, ready to work. Every industry has there own guidelines for product retouching, <br/> and our team works with almost all sectors that need complex product retouching.</p>
-                <div className='flex gap-8 justify-center items-center mt-10'>
-                    <button className='headerBtn  px-10 py-3 lg:px-20'>Free Trial</button>
-                    <button className='getAQuote px-10 py-[13px] lg:px-20'>Get A Quote</button>
-                </div>
+const HomeComponentCopy = () => {   
+
+   const [text] = useTypewriter({
+      words: ['retouching', 'photo editing', 'clipping path', 'ghost mannequin', 'deepetching'],
+      loop: 1000,
+      onLoopDone: () => console.log(`loop completed after 3 runs.`)
+   })
+   return (
+      <div className='container mx-auto'>
+         <div className='mt-[48px]'>
+            <div>
+               <h2 className='homeContainer'>Studio Metrodesk</h2>
+               <h2 className='homeContainer'>We make <span className='homeText'> <span>{text}</span><Cursor cursorColor='blue' /></span>easy!</h2>
+               <p className='mt-[16px] homeParagraph'>Photo Editing Solutions for Fashion, Apparel, E-commerce Businesses and Pro-<br /> Photographers. Our 100+ pro photo editors work 24/7 to provide you with the <br /> most personalized solutions in the market</p>
+               <div className='flex justify-center items-center gap-[16px]'>
+                  <div><button className='headerBtn block mx-auto'>Try Us Free</button></div>
+                  <div><button className='headerQuote block mx-auto'>Get A Quote</button></div>
+               </div>
+               {/* Image Section */}
+               <div className='homeImageContainer'>
+                  <div className='grid lg:grid-cols-5 gap-[16px]'>
+                     {/* Image 1 */}
+                     <div className='homeImgContainer'>
+                        <img className='mb-[16px]' src={watch} alt="This is Watch" />
+                        <img src={bag} alt="This is Bag" />
+                     </div>
+                     <div className='homeImgContainer mt-[25px]'>
+                        <img src={chair} className='mb-[16px]' alt="This is Watch" />
+                        <img src={shoes} alt="This is Bag" />
+                     </div>
+                     <div className='homeImgContainer mt-[67px]'>
+                        <img src={tshirt} className='mb-[16px]' alt="This is T-Shirt" />
+                        <img src={newWatch} className='mb-16' alt="This is Watch" />
+                     </div>
+                     <div className='homeImgContainer mt-[25px]'>
+                        <img src={children} className='mb-[16px]' alt="This is Children" />
+                        <img src={women} alt="This is Women" />
+                     </div>
+                     <div className='homeImgContainer'>
+                        <img src={sunglass} className='mb-[16px]' alt="This is Sunglass" />
+                        <img src={jewellery} alt="This is Jewellery" />
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default HomeComponentCopy;
--- src/Component/Shared/Navbar/NavMobile/NavMobile.js
+++ src/Component/Shared/Navbar/NavMobile/NavMobile.js
@@ -1,10 +1,163 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { RiMenu3Line } from "react-icons/ri";
+import { ImCross } from "react-icons/im";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+
+// import component 👇
+import Drawer from "react-modern-drawer";
+
+//import styles 👇
+import "react-modern-drawer/dist/index.css";
+
 
 const NavMobile = () => {
+  
+        // ----It is the state to open the sidebar----
+        const [isOpen, setIsOpen] = useState(false);
+        const toggleDrawer = () => {
+          // setIsOpen((prevState) => !prevState)
+          setIsOpen(!isOpen);
+        };
+      
+        // -----It is the state to expand the menu components-----
+        const [open1, setOpen1] = useState(false);
+        const toggle1 = () => {
+          setOpen1(!open1);
+        };
+      
+        const [open2, setOpen2] = useState(false);
+        const toggle2 = () => {
+          setOpen2(!open2);
+        };
+      
+        const handleClick = () => {
+          const elem = document.activeElement;
+          if (elem) {
+            elem?.blur();
+          }
+        };
+    
     return (
-        <div>
-            
-        </div>
+        <div className='block lg:hidden'>
+        <button
+          onClick={toggleDrawer}
+          className="text-4xl font-bold hover:text-gray-400 ease-in-out duration-300 w-[40px] flex justify-center items-center hover:border-r-4 hover:border-black"
+        >
+          <RiMenu3Line />
+        </button>
+        <Drawer
+          open={isOpen}
+          onClose={toggleDrawer}
+          size={300}
+          direction="right"
+          lockBackgroundScroll={false}
+          className="overflow-y-auto overflow-x-hidden"
+        >
+          <div className="w-full">
+            <div className="flex justify-end mr-8 mt-6">
+              <ImCross
+                onClick={() => setIsOpen(false)}
+                className="text-3xl font-bold hover:text-gray-400 ease-in-out duration-300 hover:cursor-pointer"
+              />
+            </div>
+  
+            <div className="w-full my-10">
+              <div className="flex flex-col items-start text-lg font-semibold gap-6 w-full h-full pl-8">
+                <Link
+                  to="/"
+                  onClick={() => setIsOpen(false)}
+                  className="uppercase hover:text-gray-400 ease-in-out duration-300"
+                >
+                  Home
+                </Link>
+  
+                <Link
+                  to="/process"
+                  onClick={() => setIsOpen(false)}
+                  className="uppercase hover:text-gray-400 ease-in-out duration-300"
+                >
+                  Process
+                </Link>
+  
+                <div className="flex gap-2">
+                  <Link
+                    to="/services"
+                    onClick={() => setIsOpen(false)}
+                    className="uppercase hover:text-gray-400 ease-in-out duration-300"
+                  >
+                    Services
+                  </Link>
+                  <div className="flex flex-col">
+                    <button onClick={toggle1} className="mt-1">
+                      {!open1 ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
+                    </button>
+                    <>
+                      {open1 && (
+                        <div className="relative right-[70px] mt-4">
+                          <div className="flex flex-col items-start w-[250px] gap-2">
+                            <Link
+                              to="/clippingpath"
+                              onClick={() => setIsOpen(false)}
+                              class="uppercase hover:text-gray-400 ease-in-out duration-300 text-base"
+                            >
+                              Clipping Path
+                            </Link>
+                            <Link
+                              to="/multiclippingpath"
+                              onClick={() => setIsOpen(false)}
+                              class="uppercase hover:text-gray-400 ease-in-out duration-300 text-base"
+                            >
+                              Multi Clipping Path
+                            </Link>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  </div>
+                </div>
+  
+                <div className="flex gap-2">
+                  <Link
+                    to="/services"
+                    onClick={() => setIsOpen(false)}
+                    className="uppercase hover:text-gray-400 ease-in-out duration-300"
+                  >
+                    Customers
+                  </Link>
+                  <div className="flex flex-col">
+                    <button onClick={toggle2} className="mt-1">
+                      {!open2 ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
+                    </button>
+                    <>
+                      {open2 && (
+                        <div className="relative right-[95px] mt-4">
+                          <div className="flex flex-col items-start w-[250px] gap-2">
+                            <Link
+                              to="/photographers"
+                              onClick={() => setIsOpen(false)}
+                              class="uppercase hover:text-gray-400 ease-in-out duration-300 text-base"
+                            >
+                              Photographers
+                            </Link>
+                            <Link
+                              to="/ecommerce"
+                              onClick={() => setIsOpen(false)}
+                              class="uppercase hover:text-gray-400 ease-in-out duration-300 text-base"
+                            >
+                              E-Commerce Businesses
+                            </Link>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Drawer>
+      </div>
     );
 };
 
