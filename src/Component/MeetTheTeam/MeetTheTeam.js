c544bbffd027fcb9305316f1ef5aac950564993c
Parvez <parvez@studiometrodesk.com>
2023-04-17 11:41:04 +0600

    Meet The Team Fixed

--- src/Component/MeetTheTeam/MeetTheTeam.js
+++ src/Component/MeetTheTeam/MeetTheTeam.js
@@ -6,8 +6,8 @@ import { FaArrowDown } from 'react-icons/fa';
 const MeetTheTeam = () => {
     return (
         <div className='about-page'>
-            <div className='container mx-auto px-10 lg:px-56'>
-                <div className='flex flex-col lg:flex-row items-center'>
+            <div className='container mx-auto px-10 lg:px-20 2xl:px-56'>
+                <div className='grid lg:grid-cols-2 items-center'>
                     <div className='flex flex-col justify-start'>
                         <h2 className='text-4xl md:text-6xl font-bold color-text pt-12 py-2 text-left'>Meet Your Awesome Team</h2>
                         <p className='block mx-auto text-xl pt-2 lg:px-0 pb-12 text-left'>Meet the people behind Studio Metrodesk. See who's editing your photos, answering your calls, enhancing your experience and constantly making sure pixel-perfect outputs.</p>
